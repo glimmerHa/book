f53f31a8c8cffa5f5ed530fd46221b24bf1060d8
yolk <50434374+glimmerHa@users.noreply.github.com>
2023-02-24 23:29:04 +0800

    Update 08_02.js

--- javascriptTheDefinitiveGuide/chapter08/08_02.js
+++ javascriptTheDefinitiveGuide/chapter08/08_02.js
@@ -12,3 +12,33 @@ var strict = (function(){
     return !this;
 }())
 console.log(strict)
+
+// 方法调用
+var calculator = {
+    operand1: 1,
+    operand2: 1,
+    add: function(){
+        // 注意this关键字的用法，this指代当前对象
+        this.result = this.operand1 + this.operand2
+    }
+}
+calculator.add()
+console.log(calculator.result)
+
+
+// 比较两行代码
+// rect.setSize(width, height)
+// setRectSize(rect, width, height)
+
+var o  ={
+    m : function(){
+        var self = this;
+        console.log(this === o)
+        f();
+        function f(){
+            console.log(this === o);
+            console.log(self === o)
+        }
+    }
+}
+o.m()
